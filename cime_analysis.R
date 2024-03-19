f3723c612adcb9292a5473bde3324ea5a34abf47
Robert Ross <raross1217@gmail.com>
2024-03-19 14:30:30 -0500

    Update graph code for aesthetics

--- cime_analysis.R
+++ cime_analysis.R
@@ -4,31 +4,59 @@ library(lubridate); library(dplyr); library(ggplot2)
 # Read data, munge, and make graph
 # Takes about 30 min to get the data from Socrata
 crime <- read.socrata(
-  "https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=Year between 2009 and 2023",
-  CREDS 
-)  %>%
-  dplyr::mutate(year = as.numeric(year), date = as.Date(date)) %>%
-  dplyr::filter(primary_type %in% c("BATTERY", "ASSAULT") & !is.na(date)) %>%
+  paste0(
+    "https://data.cityofchicago.org/resource/ijzp-q8t2.json?$",
+    "where=Ward = '48'")
+)  
+
+gd1 <- crime %>%
   dplyr::mutate(
-    week = lubridate::week(date)
+    year = as.numeric(year)
+    , date = as.Date(date)
+    , week = lubridate::week(date)
     , arrest = (arrest == "TRUE")
+    , domestic = (domestic== "TRUE")
   ) %>%
+  # dplyr::filter(primary_type == "MOTOR VEHICLE THEFT") %>%
   dplyr::group_by(week, year) %>%
   dplyr::summarise(
     arrest_rate = mean(arrest)
     , crimes = n()
     , arrests = sum(arrest)
     , date = first(date)
+    , domestic = sum(domestic)
   ) %>%
-  ggplot2::ggplot(aes(x = date, y = arrest_rate)) + geom_line() +
-  geom_vline(xintercept = as.Date("2014-10-20"), linewidth = 2, linetype = "dashed") +
-  geom_text(label = "Laquan murdered", x =  as.Date("2016-06-20"), y = .32) +
-  geom_vline(xintercept = as.Date("2020-03-09"), linewidth = 2, linetype = "dashed") +
-  geom_text(label = "COVID", x =  as.Date("2021-01-09"), y = .32) +
-  theme_minimal() +
-  labs(title = "Arrest rate for assault & battery"
-       , subtitle = "Chicago, 2009 to June 2023"
-       , caption = "Data from the City of Chicago Open Data Portal. Accessed 6/13/2023.") +
-  scale_y_continuous(labels = scales::percent_format()) +
-  xlab("Date") + ylab("Weekly arrest rate")
+  dplyr::filter(year >= 2021 & arrest_rate <= .75 & crimes >= 10) 
 
+g1 <- gd1 %>%
+  ggplot2::ggplot(aes(x = date, y = crimes)) + 
+  geom_line( linewidth = 1, linetype = "solid") +
+  geom_smooth() +
+  labs(title = "Number of crimes in the 48th ward") +
+  theme_minimal() +
+  xlab("Date (week)") +
+  ylab("Crimes") +
+  scale_y_continuous(labels = scales::comma_format())
+  
+
+g2 <- gd1 %>%
+  ggplot2::ggplot(aes(x = date, y = arrest_rate)) +   
+  geom_line( linewidth = 1, linetype = "solid") +
+  geom_smooth() +
+  labs(title = "Arrest rate in the 48th ward"
+       , caption = "Data from the City of Chicago Open Data Portal") +
+  theme_minimal() +
+  xlab("Date (week)") +
+  ylab("Arrest rate") +
+  scale_y_continuous(labels = scales::percent_format())
+
+g3 <- ggarrange(
+  g1, g2, ncol = 2
+)
+
+# Save the Plot
+jpeg(file=here::here("48th_ward_crime_stats", "plot1.jpeg"), width = 980, height = 760)
+g3
+dev.off()
+
+rm(g1, g2, g3, crime, gd1)
